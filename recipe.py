c9193b33b4fc6d5e7c2636deb59891c6b771607d
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-18 20:48:36 +0100

    Added description getter and setter and tested it
    Works so far

--- recipe.py
+++ recipe.py
@@ -1,6 +1,8 @@
 #Recipe Class
 
 class Recipe:
+
+
     def __init__(self, title):
         self.title = title
         self.description = None
@@ -8,6 +10,19 @@ class Recipe:
         self.caloriesPerPortion = None
         self.ingredients = []
         self.instructions = None
+
+    def getDescription(self):
+        return self.description
+    
+    def setDescription(self,description):
+        self.description = description
+
+# r1 = Recipe("Lasagne")
+# r1.setDescription("Layers of pasta cheese and tomato sauce")
+# print(r1.getDescription())
+
+
+
 
 
 
