1e772884d31e9b9839e8aa927427962fa0b490be
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-18 22:51:07 +0100

    Discovered display method would not work with tkinter
    get method works with tkinter may not need display method

--- recipe.py
+++ recipe.py
@@ -70,7 +70,7 @@ r1.set_ingredients("tomato")
 r1.set_ingredients("tomato")
 r1.set_ingredients("cheese")
 print(r1.get_ingredients())
-r1.remove_ingredient("cheese")
+r1.remove_ingredient(2)
 print(r1.get_ingredients())
 r1.display_ingredients()
 
@@ -78,7 +78,7 @@ r1.display_ingredients()
 window =tk.Tk()
 label = tk.Label(text = r1.get_ingredients())
 label.pack()
-label2 = tk.Label(r1.display_ingredients()) #tkinter needs text= to display so this did not work
+label2 = tk.Label(text = r1.display_ingredients()) #tkinter needs text= to display so this did not work
 label2.pack()
 window.mainloop()
 
