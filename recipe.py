79aaaf06481d10ebec7057e137895356dbecb1c7
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-18 21:13:51 +0100

    Changed naming convention
    From camel case to lowercase with underscore

--- recipe.py
+++ recipe.py
@@ -6,32 +6,32 @@ class Recipe:
     def __init__(self, title):
         self.title = title
         self.description = None
-        self.noServings = None
-        self.caloriesPerPortion = None
+        self.no_servings = None
+        self.calories_per_portion = None
         self.ingredients = []
         self.instructions = None
 
-    def getDescription(self):
+    def get_description(self):
         return self.description
     
-    def setDescription(self,description):
+    def set_description(self,description):
         self.description = description
 
-    def getNoServings(self):
+    def get_no_servings(self):
         return self.noServings
     
-    def setNoServings(self, noServings):
-        if int(noServings):
-            self.noServings = noServings
+    def set_no_servings(self, no_servings):
+        if int(no_servings):
+            self.no_servings = no_servings
             print("This is an integer")
         else:
             print("Please input an integer")
 
 
 r1 = Recipe("Lasagne")
-# r1.setDescription("Layers of pasta cheese and tomato sauce")
-# print(r1.getDescription())
-r1.setNoServings(5)
+# r1.set_description("Layers of pasta cheese and tomato sauce")
+# print(r1.get_description())
+#r1.set_no_servings(5)
  
 
 
