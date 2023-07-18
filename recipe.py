5c01cbaa4cd0b4ab52aa061b6707203ba7240922
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-18 20:39:28 +0100

    Created Recipe class with attributes

---
+++ recipe.py
@@ -0,0 +1,13 @@
+#Recipe Class
+
+class Recipe:
+    def __init__(self, title):
+        self.title = title
+        self.description = None
+        self.noServings = None
+        self.caloriesPerPortion = None
+        self.ingredients = []
+        self.instructions = None
+
+
+
