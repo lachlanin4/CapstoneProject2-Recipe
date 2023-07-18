08ed46fc54228f82a7c7b7b06685adb805dfeab5
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-18 09:45:07 +0100

    Add base classes as requested in spec

---
+++ recipes/recipe.py
@@ -0,0 +1,7 @@
+"""
+This is the recipe class
+"""
+
+
+class Recipe:
+    def __init__(self):
---
+++ recipes/recipemanager.py
@@ -0,0 +1,7 @@
+"""
+This is the recipe manager class
+"""
+
+
+class RecipeManager:
+    def __init__(self):
