8688e9082e120676a651aab44b8ecb371e26bef3
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-18 21:35:31 +0100

    Added more getters and setters and tested
    Added getter and setter for calories per person and get ingredients. Tested to check if ingredients works

--- recipe.py
+++ recipe.py
@@ -23,17 +23,39 @@ class Recipe:
     def set_no_servings(self, no_servings):
         if int(no_servings):
             self.no_servings = no_servings
-            print("This is an integer")
+            #print("This is an integer")
         else:
             print("Please input an integer")
+
+    def get_calories_per_portion(self):
+        return self.calories_per_portion
+    
+    def set_calories_per_portion(self, calories_per_portion):
+        if int(calories_per_portion):
+            self.calories_per_portion = calories_per_portion
+        else:
+            print("Please input an integer")
+    
+    def get_ingredients(self):
+        return self.ingredients
+    
+    def set_ingredients(self,ingredients):
+        self.ingredients.append(ingredients)
+        
+        
+
+
 
 
 r1 = Recipe("Lasagne")
 # r1.set_description("Layers of pasta cheese and tomato sauce")
 # print(r1.get_description())
 #r1.set_no_servings(5)
- 
+# r1.set_ingredients("tomato")
+# r1.set_ingredients("tomato")
+# print(r1.get_ingredients())
 
+ 
 
 
 
