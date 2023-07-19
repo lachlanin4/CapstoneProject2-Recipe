854de25f969efe41f85642d24b953153aee93df1
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-19 17:01:43 +0100

    Add basic ingredient class construction

---
+++ ingredient.py
@@ -0,0 +1,15 @@
+# Ingredient class definition
+class Ingredient:
+    """This class encapsulates what is in an ingredient as part of a recipe.
+    it contains a name for the ingredient, the amount required, the units associated with the amount,
+    a list of alergens that the ingredient might contain e.g. wheat and the number of callories
+    associated with the amount."""
+
+    def __init__(
+        self, name: str, amount: float, units: str, alergens: [str], callories: float
+    ):
+        self._name_of_ingredient = name
+        self._amount = amount
+        self._units = units
+        self._alergens = alergens
+        self._callories = callories
