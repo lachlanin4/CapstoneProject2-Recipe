cd05cb52ccecf6f92af785c7df03b5dfe6bc764e
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-07-20 22:31:03 +0100

    Did I push it yesterday?
    Because I clearly see on GitHub desktop that I don't"

---
+++ recipe_menager.py
@@ -0,0 +1,27 @@
+class RecipeManager:
+    def __init__(self):
+        self.recipes = []
+
+    @property
+    def display_recipes(self):
+        for recipe in self.recipes:
+            print(recipe)
+            return self.recipes
+            
+    @display_recipes.getter
+    def display_recipes(self):
+      return str(self.recipes)
+
+    @display_recipes.setter
+    def add_recipe(self, recipe):
+        self.recipes.append(recipe)
+
+    def update_recipe(self, recipe_index, new_recipe):
+        self.recipes[recipe_index] = new_recipe
+    
+    def delete_recipe(self, recipe_index):
+        self.recipes.pop(recipe_index)
+
+
+
+
