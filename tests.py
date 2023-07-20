dde44ec8c95a4057c9bfe1d50848259028460d4f
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-21 00:21:37 +0100

    Import the manager so you don't have to replicate

--- tests.py
+++ tests.py
@@ -1,36 +1,5 @@
-class RecipeManager:
-    def __init__(self):
-        self.recipes = []
-
-    @property
-    def display_recipes(self):
-        for recipe in self.recipes:
-          print(recipe)
-        return self.recipes
-          
-    @display_recipes.getter
-    def display_recipes(self):
-      return str(self.recipes)
-
-    @display_recipes.setter
-    def add_recipe(self, recipe):
-        self.recipes.append(recipe)
-        return str(recipe)
-
-
-  
-
-    def update_recipe(self, recipe_index, new_recipe):
-        self.recipes[recipe_index] = new_recipe
-        return str(new_recipe)
-
-
-    def delete_recipe(self, recipe_index):
-        self.recipes.pop(recipe_index)
-        return str(recipe_index)
-
-
-
+from recipe_Menager import RecipeManager
+from recipe import Recipe
 
 # Test the code
 recipe_manager = RecipeManager()
