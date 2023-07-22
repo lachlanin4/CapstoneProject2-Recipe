ed7cda41693325baa5854fb16e85ffd1cf679452
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-22 17:36:01 +0100

    Added lots of buttons and a scroll bar
    turned listbox into a function and added a scroll and created lots of buttons

--- GUI.py
+++ GUI.py
@@ -1,9 +1,11 @@
 import tkinter as tk
-#from recipe import Recipe
+from tkinter import ttk
+from recipe import Recipe
 from recipemanager import RecipeManager
+import tkinter.filedialog
 #from recipemanager import Recipe
 
-
+"""
 # class Main_Application(tk.Frame):
 #     def __init__(self,parent,*args,**kwargs):
 #         tk.Frame.__init__(self,parent,*args,**kwargs)
@@ -13,32 +15,76 @@ from recipemanager import RecipeManager
 #     root = tk.Tk()
 #     Main_Application(root).pack(side="top", fill="both", expand=True)
 #     root.mainloop()
+"""
+
+   
+root = tk.Tk()
+#lbl_recipe_listbox = tk.Label(text="Here are your recipes:")
 recipe_manager = RecipeManager()
 recipe_manager.recipes = ['pie', 'tart', 'lasagne']
-#Create the root
-root = tk.Tk()
-root.title('Listbox')
+
+def display_recipes():
+    main_listbox()
 
 #Create a Listbox
-list_items=tk.Variable(value= recipe_manager.recipes)
-listbox = tk.Listbox(
-    root,
-    height = 10,
-    listvariable = list_items,
-    selectmode=tk.SINGLE
-)
-#Formatting listbox so it fills screen
-listbox.pack(expand=True, fill=tk.X)
+def main_listbox():
+    list_items=tk.Variable(value= recipe_manager.recipes)
+    listbox = tk.Listbox(
+        root,
+        height = 10,
+        listvariable = list_items,
+        selectmode=tk.SINGLE
+    )
+    #Formatting listbox so it fills screen
+    listbox.pack(expand=True,fill=tk.BOTH, side=tk.LEFT)
 
-"""
-##This was meant to let you select the recipe but listbox.curselection() returns a tuple making it difficult
+    scrollbar = ttk.Scrollbar(
+        root,
+        orient=tk.VERTICAL,
+        command=listbox.yview
+    )
+    listbox["yscrollcommand"] = scrollbar.set
+    scrollbar.pack(side=tk.LEFT,expand=True, fill=tk.Y)
+
+
 # def items_selected(event):
 #     #gets selected item
-#     selected_recipe  = listbox.curselection()
-#     tk.Label(text=(f"You have selected{recipe_manager.recipes[selected_recipe[0]]}"))
-#listbox.bind('<<ListboxSelect>>', callback)
-"""
-requested_recipe = tk.Entry(fg="blue",bg = "pink", width=50)
-requested_recipe.pack()
+#     selected_indices  = listbox.curselection()
+#     selected_recipes=",".join([listbox.get(i) for i in selected_indices])
+#     msg=f"You selected: {selected_recipes}"
 
+#     showinfo(title="Information",message=msg)
+# listbox.bind('<<ListboxSelect>>', items_selected)
+
+
+#The below is direcetory selector on uml but need to look into filedialog again
+display_recipes()
+tkinter.filedialog.askdirectory()
+ent_recipe = tk.Entry(fg="blue",bg = "pink", width=50)
+ent_recipe.pack()
+
+requested_recipe = ent_recipe.get()
+
+btn_recipe=tk.Button(text="Submit",command="")
+btn_recipe.pack()
+
+#recipe_display:tkinter.modal dialog box
+#recipe_edit same as above
+
+#All the buttons and packed them
+btn_select_to_remove = tk.Button(text= "Remove Recipe",command="")
+btn_select_to_remove.pack()
+btn_select_to_search = tk.Button(text="Search",command="")
+btn_select_to_search.pack()
+btn_select_to_add = tk.Button(text="Add Recipe",command="")
+btn_select_to_add.pack()
+btn_select_to_display_recipe = tk.Button(text="Display Recipe",command="")
+btn_select_to_display_recipe.pack()
+btn_select_to_edit_recipe = tk.Button(text="Edit Recipe", command="")
+btn_select_to_display_recipe.pack()
+
+# def display_recipe():
+
+
+    
 root.mainloop()
