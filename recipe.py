061cb98273b3c65827b8f5d0696f9346e914e6a0
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-23 15:57:34 +0100

    Use black formatting on recipe class

--- recipe.py
+++ recipe.py
@@ -1,9 +1,7 @@
 from ingredient import Ingredient
 
-#Recipe Class
+# Recipe Class
 class Recipe:
-
-
     def __init__(self, title):
         self._title = title
         self._description = None
@@ -17,7 +15,7 @@ class Recipe:
         return self._description
 
     @description.setter
-    def description(self,description):
+    def description(self, description):
         self._description = description
 
     @property
@@ -25,7 +23,7 @@ class Recipe:
         return self._no_servings
 
     @no_servings.setter
-    def no_servings(self, no_servings:int):
+    def no_servings(self, no_servings: int):
         self._no_servings = no_servings
 
     @property
@@ -33,7 +31,7 @@ class Recipe:
         return self._calories_per_portion
 
     @calories_per_portion.setter
-    def calories_per_portion(self, calories_per_portion:int):
+    def calories_per_portion(self, calories_per_portion: int):
         self._calories_per_portion = calories_per_portion
 
     @property
@@ -41,34 +39,34 @@ class Recipe:
         return self._ingredients
 
     @ingredients.setter
-    def ingredients(self,ingredients:list[Ingredient]):
+    def ingredients(self, ingredients: list[Ingredient]):
         self._ingredients = ingredients
 
     # Method to progressively extend ingredients list
-    def add_ingredients(self,ingredients:list[Ingredient]):
+    def add_ingredients(self, ingredients: list[Ingredient]):
         if len(self._ingredients) > 0:
-           self._ingredients.extend(ingredients)
+            self._ingredients.extend(ingredients)
         else:
-           self._ingredients = ingredients
+            self._ingredients = ingredients
 
     # Method to progressively remove ingredients from list
-    def remove_ingredient(self,ingredient:Ingredient):
+    def remove_ingredient(self, ingredient: Ingredient):
         found = False
         for x, rec in enumerate(self._ingredients):
-          if rec.name == ingredient.name:
-            del self._ingredients[x]
-            found = True
-            break
+            if rec.name == ingredient.name:
+                del self._ingredients[x]
+                found = True
+                break
 
         if not found:
-          raise("I couldn't find the recipe")
+            raise ("I couldn't find the recipe")
 
     @property
     def instructions(self):
         return self._instructions
 
     @instructions.setter
-    def instructions(self, instructions:list[str]):
+    def instructions(self, instructions: list[str]):
         self._instructions = instructions
 
     # Method to progressively remove ingredients from list
