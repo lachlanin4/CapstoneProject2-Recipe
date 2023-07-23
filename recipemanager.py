4f1d8b378387b43ed87f95e1b51b242b6cf90f6b
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-23 20:10:03 +0100

    Add method to read recipe files from a directory

--- recipemanager.py
+++ recipemanager.py
@@ -2,7 +2,7 @@ from ingredient import Ingredient
 from recipe import Recipe
 from typing import List
 from glob import glob
-from yaml import load, dump
+from yaml import load, dump, Dumper, Loader
 from pathlib import Path
 
 class RecipeManager:
@@ -29,3 +29,34 @@ class RecipeManager:
     
     def delete_recipe(self, recipe_to_delete: Recipe):
         self.recipes.remove(recipe_to_delete)
+
+    def read_recipes_from_files(self, path:Path):
+
+        # Create a list to contain returned objects from reading files
+        data_sets = []
+
+        # Get a list of yml files that are already in the path
+        yaml_files = path.glob("*.yml")
+
+        print(f"{path.absolute}")
+
+        # Iterate a list of yaml file previously found in the route path
+        for recipeFile in yaml_files:
+
+            # Open the file as read only
+            f = open(recipeFile, 'r')
+
+            # Read the file
+            stream = f.read()
+
+            # Load the data
+            data = load(stream, Loader=Loader)
+
+            # Append the data
+            data_sets.append(data)
+
+            # Close the file
+            f.close()
+
+        if len(data_sets) > 0:
+            self.recipes = data_sets
