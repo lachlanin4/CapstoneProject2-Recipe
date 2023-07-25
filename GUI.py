d57f5a97ceddbba79e313686be573f1161a136a4
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-25 16:29:42 +0100

    Changed layout and added a function
    function is display recipes

--- GUI.py
+++ GUI.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-#from recipe import Recipe
+from recipe import Recipe
 from RecipeManager import RecipeManager
-# from RecipeManager import RecipeManager
 import tkinter.filedialog
 # from RecipeManager import Recipe
 
@@ -22,7 +21,6 @@ import tkinter.filedialog
 root = tk.Tk()
 root.title("Recipe Menager")
 listbox:None # I am not defining this variable
-#lbl_recipe_listbox = tk.Label(text="Here are your recipes:")
 # recipe_manager = RecipeManager() I put it in comment
 recipe_manager = RecipeManager()
 
@@ -43,6 +41,8 @@ def main_listbox():
     )
     #Formatting listbox so it fills screen
     listbox.pack(expand=True,fill=tk.BOTH, side=tk.LEFT)
+    #listbox.pack_forget()
+    #listbox.pack(expand=True,fill=tk.BOTH, side=tk.LEFT) #this was to try and refresh the 
 
     scrollbar = ttk.Scrollbar(
         master=main_frame,
@@ -56,9 +56,10 @@ def main_listbox():
     def items_selected(event):
         #gets selected item
         selected_indices  = listbox.curselection()
+        global selected_recipes
         selected_recipes=",".join([listbox.get(i) for i in selected_indices])
         msg=f"You selected: {selected_recipes}"
-        chosen_item = tk.Label(text=msg)
+        chosen_item = tk.Label(master=main_frame,text=msg)
         chosen_item.pack()
 
     listbox.bind('<<ListboxSelect>>', items_selected)
@@ -87,6 +88,10 @@ def edit_recipe():
         recipe_manager.edit_recipe(index_to_edit, new_title)
         main_listbox()
 
+def display_recipe():
+    recipe_description = tk.Label(master=display_and_add_frame,text=selected_recipes.get_description)
+    recipe_ingredients = tk.Label(master=display_and_add_frame,text=selected_recipes.get_ingredients)
+    recipe_instructions= tk.Label(master=display_and_add_frame,text=selected_recipes.get_instructions)
 
 
 
@@ -99,18 +104,22 @@ title_frame = tk.Frame(master=entire_frame,relief=tk.RIDGE, borderwidth=10)
 title_frame.pack()
 editing_frame = tk.Frame(master=entire_frame, relief=tk.RIDGE, borderwidth=5)
 editing_frame.pack()
+entry_frame=tk.Frame(master=entire_frame, relief=tk.RIDGE, borderwidth=5)
+entry_frame.pack()
 main_frame = tk.Frame(master=entire_frame,relief=tk.RIDGE, borderwidth=5)
 main_frame.pack()
 recipe_frame= tk.Frame(master=entire_frame,relief=tk.RIDGE,borderwidth=5)
 recipe_frame.pack()
+display_and_add_frame =tk.Frame(master=entire_frame,relief=tk.RIDGE,borderwidth=5)
+display_and_add_frame.pack()
+
 
 
 
 #Recipes is the title that is going to go above everything
-lbl_recipe_listbox = tk.Label(master=title_frame,text="Recipes") ##
-lbl_recipe_listbox.pack(padx=5,pady=5) ##
-# display_recipes()
-# display_recipes()
+lbl_recipe_listbox = tk.Label(master=title_frame,text="Recipes") #Title
+lbl_recipe_listbox.pack(padx=5,pady=5)
+
 #tkinter.filedialog.askdirectory() #This pops up the file explorer
 
 #recipe_display:tkinter.modal dialog box
@@ -128,10 +137,11 @@ btn_select_to_display_recipe.grid(row=0,column=0)
 btn_select_to_edit_recipe = tk.Button(master=recipe_frame,text="Edit Recipe", command=edit_recipe)
 btn_select_to_edit_recipe.grid(row=0,column=1)
 
+#Boxes to display recipe description, ingredients and instructions
+
 #Ent_recipe is going to go with when user wants to add recipe name, add ingredients, add instructions
-ent_recipe = tk.Entry(master=editing_frame,fg="blue",bg = "pink", width=30)
-#ent_recipe.pack()
-requested_recipe = ent_recipe.get()
+ent_recipe = tk.Entry(master=entry_frame,fg="blue",bg = "pink", width=30)
+ent_recipe.pack()
 
 btn_recipe=tk.Button(master=editing_frame, text="Submit",command="")
 #btn_recipe.pack()
